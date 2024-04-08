9fe000c943fe36af6e56828f9236fdd4a625d68c
Administrador <Administrador@DESKTOP-ANA>
2024-04-08 02:40:04 -0300

    Agregar archivos de proyecto.

---
+++ CRUDUser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDUser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDUser/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDUser.Models;
+
+public partial class Usuario
+{
+    public int Idusuario { get; set; }
+
+    public string? NameUsuario { get; set; }
+}
